531d613c8e8ab514095d56535bc9d207bca2f813
LauLyman <2718390374@qq.com>
2017-11-10 15:37:18 +0800

    LauLyman
    V36

--- Assets/Editor/AutoMakeSceneConfig.cs
+++ Assets/Editor/AutoMakeSceneConfig.cs
@@ -17,8 +17,10 @@ public class AutoMakeSceneConfig
         SceneInfo sceneInfo = new SceneInfo();
         int count = 0;
         int j;
+        
         bool isHaveTerrain = false;
         GameObject nonblocks = null;
+        GameObject terrainGo = null;
         for (int i = 0; i < gos.Length; ++i)
         {
             GameObject go = gos[i];
@@ -38,10 +40,12 @@ public class AutoMakeSceneConfig
                 }
                 isHaveTerrain = true;
                 ts = go.transform.GetChild(0);
+                terrainGo = ts.gameObject;
                 if (ts.tag != GameObjectTags.Terrain)
                 {
                     throw new System.Exception("地形节点下有名称为:" + ts.name + "的GameObject的Tag不为terrain,请重新设置！！！");
                 }
+
                 Collider collider = ts.GetComponent<Collider>();
                 if (collider == null)
                 {
@@ -124,11 +128,70 @@ public class AutoMakeSceneConfig
         {
             nonblocks.SetActive(false);
         }
+        if (terrainGo != null)
+        {
+            MeshRenderer meshRender = terrainGo.GetComponent<MeshRenderer>();
+            Vector3 vector3 = meshRender.bounds.size;
+            int xLenght = 0;
+            int zLength = 0;
+            if (vector3.x % 2f != 0f)
+            {
+                xLenght = (int)vector3.x + 1;
+                if (xLenght % 2 != 0)
+                {
+                    xLenght++;
+                }
+            }
+            if (vector3.z % 2f != 0f)
+            {
+                zLength = (int)vector3.z + 1;
+                if (zLength % 2 != 0)
+                {
+                    zLength++;
+                }
+            }
 
+            byte[,] grids = new byte[xLenght, zLength];
+
+            for (int i = -xLenght / 2; i < xLenght; ++i)
+            {
+                for (j = -zLength / 2; j < zLength; ++j)
+                {
+                    int x = i + xLenght / 2;
+                    float xx = i + 0.5f;
+
+                    int y = j + zLength / 2;
+                    float yy = j + 0.5f;
+
+                    //todo 射线检测
+                    
+                    RaycastHit raycastHit;
+                    Vector3 startPosition = new Vector3(xx, 10f, yy);
+                    Ray ray = new Ray(startPosition, Vector3.down);
+                    bool isHit = Physics.Raycast(ray, out raycastHit, 20f);
+                    if (isHit)
+                    {
+                        if (raycastHit.transform.tag == GameObjectTags.Block)
+                        {
+                            grids[x, y] = 0;
+                        }
+                        else
+                        {
+                            grids[x, y] = 1;
+                        }
+                    }
+                    else
+                    {
+                        grids[x, y] = 0;
+                    }
+                }
+            }
+            Debug.Log(grids);
+        }
         
 
 
-        SaveConfig(sceneInfo, scene.path);
+        //SaveConfig(sceneInfo, scene.path);
     }
 
 
