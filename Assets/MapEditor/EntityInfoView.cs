fdf526d20e39d0b4e8a494243be51a12f8a6ed44
LIUQIANYUAN-PC\Administrator <liuqianyuan@kingsoft.com>
2018-02-06 10:38:14 +0800

    LauLyman
    v58

--- Assets/MapEditor/EntityInfoView.cs
+++ Assets/MapEditor/EntityInfoView.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UIWidgets;
 public class EntityInfoView
 {
     public static EntityInfoView Instance
@@ -16,9 +17,19 @@ public class EntityInfoView
     public static EntityInfoView entityInfoView;
 
     private Transform container;
+    private Text entityPositionText;
+    private Text entityRotationText;
+    private Combobox entityTypeCombobox;
+    private Combobox entityResPathCombobox;
+
+
     public void Init(Transform transform)
     {
         container = transform;
+        entityPositionText = container.Find("entityPositionText").GetComponent<Text>();
+        entityRotationText = container.Find("entityRotationText").GetComponent<Text>();
+        entityTypeCombobox = container.Find("entityTypeCombobox/Combobox").GetComponent<Combobox>();
+        entityResPathCombobox = container.Find("entityResPathCombobox/Combobox").GetComponent<Combobox>();
     }
 
     public void Show()
